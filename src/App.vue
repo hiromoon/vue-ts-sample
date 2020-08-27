cfa85e8d6824910e5143f5291da290df37c76d5f
hiromoon <hiromoon0428@gmail.com>
2020-08-27 09:45:41 +0900

    デフォルトのレイアウトを設定

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
   <v-app>
-    <router-view />
+    <v-navigation-drawer app>
+      <ul>
+        <li>Users</li>
+      </ul>
+    </v-navigation-drawer>
+    <v-app-bar app>
+      <h1>Header</h1>
+    </v-app-bar>
+    <v-main>
+      <v-container fluid>
+        <router-view />
+      </v-container>
+    </v-main>
   </v-app>
 </template>
 
