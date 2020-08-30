c2836c0489e9cea8993db83870ad1720931656f6
hiromoon <hiromoon0428@gmail.com>
2020-08-31 04:00:44 +0900

    テーブルで User 一覧を表示できるようにした

--- src/pages/users/UsersIndexPage.vue
+++ src/pages/users/UsersIndexPage.vue
@@ -1,17 +1,54 @@
 <template>
   <div>
-    <h1>Users</h1>
-    <router-link to="/users">
-      index
-    </router-link>
-    <router-link to="/users/new">
-      new
-    </router-link>
-    <router-link to="/users/10">
-      show
-    </router-link>
-    <router-link to="/users/10/edit">
-      edit
-    </router-link>
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+          <th class="text-left">id</th>
+          <th class="text-left">name</th>
+          <th class="text-left">email</th>
+        </thead>
+        <tbody>
+          <tr
+            v-for="user in users"
+            :key="user.id"
+            @click="navigateToShow(user.id)"
+          >
+            <td>{{ user.id }}</td>
+            <td>{{ user.name }}</td>
+            <td>{{ user.email }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      users: null
+    };
+  },
+  created() {
+    this.fetchUsers();
+  },
+  methods: {
+    async fetchUsers() {
+      try {
+        const res = await fetch("http://localhost:3000/users");
+        const payload = await res.json();
+        this.users = payload;
+      } catch (err) {
+        console.error(err);
+      }
+    },
+
+    navigateToShow(userId: string) {
+      this.$router.push({ path: "/users/" + userId });
+    }
+  }
+});
+</script>
