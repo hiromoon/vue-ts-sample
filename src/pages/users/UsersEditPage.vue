9685de3d99d97273651fa16e1a6b2427a590fa66
hiromoon <hiromoon0428@gmail.com>
2020-08-31 04:01:00 +0900

    ユーザの編集ページを追加

--- src/pages/users/UsersEditPage.vue
+++ src/pages/users/UsersEditPage.vue
@@ -1,5 +1,16 @@
 <template>
-  <h1>Users > {{ id }} Edit Page</h1>
+  <div>
+    <h1>Users > {{ id }} Edit Page</h1>
+
+    <v-form v-model="valid">
+      <v-text-field v-model="user.id" label="id" disabled></v-text-field>
+      <v-text-field v-model="user.name" label="Name"></v-text-field>
+      <v-text-field v-model="user.email" label="Email"></v-text-field>
+      <v-btn color="success" class="mr-4" @click="updateUser">
+        Save
+      </v-btn>
+    </v-form>
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,6 +18,45 @@ import Vue from "vue";
 
 export default Vue.extend({
   name: "UserEditPage",
-  props: ["id"]
+  props: { id: String },
+  data() {
+    return {
+      valid: false,
+      user: {
+        id: null,
+        name: null,
+        email: null
+      }
+    };
+  },
+  created() {
+    this.fetchUser();
+  },
+  methods: {
+    async fetchUser() {
+      try {
+        const res = await fetch(`http://localhost:3000/users/${this.id}`);
+        const payload = await res.json();
+        this.user = payload;
+      } catch (err) {
+        console.error(err);
+      }
+    },
+    async updateUser() {
+      try {
+        const res = await fetch(`http://localhost:3000/users/${this.id}`, {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(this.user),
+          mode: "cors"
+        });
+        this.user = await res.json();
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
 });
 </script>
